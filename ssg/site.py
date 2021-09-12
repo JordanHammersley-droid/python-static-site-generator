ce890890726723438d93f597e36463297ac5fdfb
JordanHammersley-droid <hammersleyjordan@gmail.com>
2021-09-12 17:35:17 +0100

    Update site.py

---
+++ ssg/site.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+class Site:
+    def __init__(self, source, dest):
+        Path(self.source) = source
+        Path(dest.source) = dest
+
+    def create_dir(self, path):
+        directory = 
